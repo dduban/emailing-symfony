e46249e193172d5d2c21de936e9b85b317f92856
Dominika Dubaniowska <dominika.dubaniowska@gmail.com>
2021-08-09 19:05:16 +0200

    Validation

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,12 +3,15 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Doctrine\DBAL\Types\IntegerType;
 use Doctrine\DBAL\Types\StringType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 use Symfony\Component\Validator\Constraints\Unique;
 
 class UserType extends AbstractType
@@ -16,12 +19,22 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('name')
-            ->add('surname')
-            ->add('phone')
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Unique()
+                ]
+            ])
+            ->add('name', StringType::class, ['constraints' => new NotBlank()] )
+            ->add('surname', StringType::class, ['constraints' => new NotBlank()])
+            ->add('phone', IntegerType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Regex("/^[1-9].[0-9]{8}/")
+                ]
+            ])
             ->add('birthday')
-            ->add('is_confirm');
+        ;
         $builder
             ->add('alerts', CollectionType::class, [
                 'entry_type' => AlertType::class,
